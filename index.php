20bc8f5499d1390eb77f2daf8df7cabc67f8408c
panggah10 <panggah.widiandana10@gmail.com>
2026-03-02 16:50:23 +0700

    Menambahkan form login

--- index.php
+++ index.php
@@ -2,5 +2,10 @@
 
 echo "Hello World";
 <form method=POST action="login.php">
+<label for="username">Username:</label>
+<input type="text" id="username" name="username">
+<label for="password">Password:</label>
+<input type="password" id="password" name="password">
+	
   </form>
 ?>
